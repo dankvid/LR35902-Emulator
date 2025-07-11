bb16e457fbaebad71e0fe9398ecb970e8e16f11f
David Genzler <david.genzler@smail.th-koeln.de>
2025-07-11 18:20:25 +0200

    Implement load operations for additional registers and update testing log

--- src/cpu.c
+++ src/cpu.c
@@ -135,7 +135,11 @@ void op_load_b_d(CPU* cpu) { cpu->b = cpu->d; cpu->cycles += 4; }
 void op_load_b_e(CPU* cpu) { cpu->b = cpu->e; cpu->cycles += 4; }
 void op_load_b_h(CPU* cpu) { cpu->b = cpu->h; cpu->cycles += 4; }
 void op_load_b_l(CPU* cpu) { cpu->b = cpu->l; cpu->cycles += 4; }
-// TODO HL
+void op_load_b_hlp(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    cpu->b = mem_read(hl);
+    cpu->cycles += 8;
+}
 void op_load_b_a(CPU* cpu) { cpu->b = cpu->a; cpu->cycles += 4; }
 
 void op_load_c_b(CPU* cpu) { cpu->c = cpu->b; cpu->cycles += 4; }
@@ -144,8 +148,124 @@ void op_load_c_d(CPU* cpu) { cpu->c = cpu->d; cpu->cycles += 4; }
 void op_load_c_e(CPU* cpu) { cpu->c = cpu->e; cpu->cycles += 4; }
 void op_load_c_h(CPU* cpu) { cpu->c = cpu->h; cpu->cycles += 4; }
 void op_load_c_l(CPU* cpu) { cpu->c = cpu->l; cpu->cycles += 4; }
-// TODO HL
+void op_load_c_hlp(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    cpu->c = mem_read(hl);
+    cpu->cycles += 8;
+}
 void op_load_c_a(CPU* cpu) { cpu->c = cpu->a; cpu->cycles += 4; }
+
+void op_load_d_b(CPU* cpu) { cpu->d = cpu->b; cpu->cycles += 4; }
+void op_load_d_c(CPU* cpu) { cpu->d = cpu->c; cpu->cycles += 4; }
+void op_load_d_d(CPU* cpu) { cpu->d = cpu->d; cpu->cycles += 4; }
+void op_load_d_e(CPU* cpu) { cpu->d = cpu->e; cpu->cycles += 4; }
+void op_load_d_h(CPU* cpu) { cpu->d = cpu->h; cpu->cycles += 4; }
+void op_load_d_l(CPU* cpu) { cpu->d = cpu->l; cpu->cycles += 4; }
+void op_load_d_hlp(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    cpu->d = mem_read(hl);
+    cpu->cycles += 8;
+}
+void op_load_d_a(CPU* cpu) { cpu->d = cpu->a; cpu->cycles += 4; }
+
+void op_load_e_b(CPU* cpu) { cpu->e = cpu->b; cpu->cycles += 4; }
+void op_load_e_c(CPU* cpu) { cpu->e = cpu->c; cpu->cycles += 4; }
+void op_load_e_d(CPU* cpu) { cpu->e = cpu->d; cpu->cycles += 4; }
+void op_load_e_e(CPU* cpu) { cpu->e = cpu->e; cpu->cycles += 4; }
+void op_load_e_h(CPU* cpu) { cpu->e = cpu->h; cpu->cycles += 4; }
+void op_load_e_l(CPU* cpu) { cpu->e = cpu->l; cpu->cycles += 4; }
+void op_load_e_hlp(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    cpu->e = mem_read(hl);
+    cpu->cycles += 8;
+}
+void op_load_e_a(CPU* cpu) { cpu->e = cpu->a; cpu->cycles += 4; }
+
+void op_load_h_b(CPU* cpu) { cpu->h = cpu->b; cpu->cycles += 4; }
+void op_load_h_c(CPU* cpu) { cpu->h = cpu->c; cpu->cycles += 4; }
+void op_load_h_d(CPU* cpu) { cpu->h = cpu->d; cpu->cycles += 4; }
+void op_load_h_e(CPU* cpu) { cpu->h = cpu->e; cpu->cycles += 4; }
+void op_load_h_h(CPU* cpu) { cpu->h = cpu->h; cpu->cycles += 4; }
+void op_load_h_l(CPU* cpu) { cpu->h = cpu->l; cpu->cycles += 4; }
+void op_load_h_hlp(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    cpu->h = mem_read(hl);
+    cpu->cycles += 8;
+}
+void op_load_h_a(CPU* cpu) { cpu->h = cpu->a; cpu->cycles += 4; }
+
+void op_load_l_b(CPU* cpu) { cpu->l = cpu->b; cpu->cycles += 4; }
+void op_load_l_c(CPU* cpu) { cpu->l = cpu->c; cpu->cycles += 4; }
+void op_load_l_d(CPU* cpu) { cpu->l = cpu->d; cpu->cycles += 4; }
+void op_load_l_e(CPU* cpu) { cpu->l = cpu->e; cpu->cycles += 4; }
+void op_load_l_h(CPU* cpu) { cpu->l = cpu->h; cpu->cycles += 4; }
+void op_load_l_l(CPU* cpu) { cpu->l = cpu->l; cpu->cycles += 4; }
+void op_load_l_hlp(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    cpu->l = mem_read(hl);
+    cpu->cycles += 8;
+}
+void op_load_l_a(CPU* cpu) { cpu->l = cpu->a; cpu->cycles += 4; }
+
+void op_load_hlp_b(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    mem_write(hl, cpu->b);
+    cpu->cycles += 8;
+}
+
+void op_load_hlp_c(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    mem_write(hl, cpu->c);
+    cpu->cycles += 8;
+}
+
+void op_load_hlp_d(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    mem_write(hl, cpu->d);
+    cpu->cycles += 8;
+}
+
+void op_load_hlp_e(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    mem_write(hl, cpu->e);
+    cpu->cycles += 8;
+}
+
+void op_load_hlp_h(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    mem_write(hl, cpu->h);
+    cpu->cycles += 8;
+}
+
+void op_load_hlp_l(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    mem_write(hl, cpu->l);
+    cpu->cycles += 8;
+}
+
+void op_halt(CPU* cpu) {
+    cpu->halted = 1;
+    cpu->cycles += 4;
+}
+
+void op_load_hlp_a(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    mem_write(hl, cpu->a);
+    cpu->cycles += 8;
+}
+
+void op_load_a_b(CPU* cpu) { cpu->a = cpu->b; cpu->cycles += 4; }
+void op_load_a_c(CPU* cpu) { cpu->a = cpu->c; cpu->cycles += 4; }
+void op_load_a_d(CPU* cpu) { cpu->a = cpu->d; cpu->cycles += 4; }
+void op_load_a_e(CPU* cpu) { cpu->a = cpu->e; cpu->cycles += 4; }
+void op_load_a_h(CPU* cpu) { cpu->a = cpu->h; cpu->cycles += 4; }
+void op_load_a_l(CPU* cpu) { cpu->a = cpu->l; cpu->cycles += 4; }
+void op_load_a_hlp(CPU* cpu) {
+    uint16_t hl = (cpu->h << 8) | cpu->l;
+    cpu->a = mem_read(hl);
+    cpu->cycles += 8;
+}
+void op_load_a_a(CPU* cpu) { cpu->a = cpu->a; cpu->cycles += 4; }
 
 void init_opcode_table() {
     memset(opcode_table, 0, sizeof(opcode_table));
@@ -172,6 +292,7 @@ void init_opcode_table() {
     REGISTER_OPCODE(0x43, op_load_b_e);
     REGISTER_OPCODE(0x44, op_load_b_h);
     REGISTER_OPCODE(0x45, op_load_b_l);
+    REGISTER_OPCODE(0x46, op_load_b_hlp);
     REGISTER_OPCODE(0x47, op_load_b_a);
     REGISTER_OPCODE(0x48, op_load_c_b);
     REGISTER_OPCODE(0x49, op_load_c_c);
@@ -179,10 +300,64 @@ void init_opcode_table() {
     REGISTER_OPCODE(0x4B, op_load_c_e);
     REGISTER_OPCODE(0x4C, op_load_c_h);
     REGISTER_OPCODE(0x4D, op_load_c_l);
+    REGISTER_OPCODE(0x4E, op_load_c_hlp);
     REGISTER_OPCODE(0x4F, op_load_c_a);
+    REGISTER_OPCODE(0x50, op_load_d_b);
+    REGISTER_OPCODE(0x51, op_load_d_c);
+    REGISTER_OPCODE(0x52, op_load_d_d);
+    REGISTER_OPCODE(0x53, op_load_d_e);
+    REGISTER_OPCODE(0x54, op_load_d_h);
+    REGISTER_OPCODE(0x55, op_load_d_l);
+    REGISTER_OPCODE(0x56, op_load_d_hlp);
+    REGISTER_OPCODE(0x57, op_load_d_a);
+    REGISTER_OPCODE(0x58, op_load_e_b);
+    REGISTER_OPCODE(0x59, op_load_e_c);
+    REGISTER_OPCODE(0x5A, op_load_e_d);
+    REGISTER_OPCODE(0x5B, op_load_e_e);
+    REGISTER_OPCODE(0x5C, op_load_e_h);
+    REGISTER_OPCODE(0x5D, op_load_e_l);
+    REGISTER_OPCODE(0x5E, op_load_e_hlp);
+    REGISTER_OPCODE(0x5F, op_load_e_a);
+    REGISTER_OPCODE(0x60, op_load_h_b);
+    REGISTER_OPCODE(0x61, op_load_h_c);
+    REGISTER_OPCODE(0x62, op_load_h_d);
+    REGISTER_OPCODE(0x63, op_load_h_e);
+    REGISTER_OPCODE(0x64, op_load_h_h);
+    REGISTER_OPCODE(0x65, op_load_h_l);
+    REGISTER_OPCODE(0x66, op_load_h_hlp);
+    REGISTER_OPCODE(0x67, op_load_h_a);
+    REGISTER_OPCODE(0x68, op_load_l_b);
+    REGISTER_OPCODE(0x69, op_load_l_c);
+    REGISTER_OPCODE(0x6A, op_load_l_d);
+    REGISTER_OPCODE(0x6B, op_load_l_e);
+    REGISTER_OPCODE(0x6C, op_load_l_h);
+    REGISTER_OPCODE(0x6D, op_load_l_l);
+    REGISTER_OPCODE(0x6E, op_load_l_hlp);
+    REGISTER_OPCODE(0x6F, op_load_l_a);
+    REGISTER_OPCODE(0x70, op_load_hlp_b);
+    REGISTER_OPCODE(0x71, op_load_hlp_c);
+    REGISTER_OPCODE(0x72, op_load_hlp_d);
+    REGISTER_OPCODE(0x73, op_load_hlp_e);
+    REGISTER_OPCODE(0x74, op_load_hlp_h);
+    REGISTER_OPCODE(0x75, op_load_hlp_l);
+    REGISTER_OPCODE(0x76, op_halt);
+    REGISTER_OPCODE(0x77, op_load_hlp_a);
+    REGISTER_OPCODE(0x78, op_load_a_b);
+    REGISTER_OPCODE(0x79, op_load_a_c);
+    REGISTER_OPCODE(0x7A, op_load_a_d);
+    REGISTER_OPCODE(0x7B, op_load_a_e);
+    REGISTER_OPCODE(0x7C, op_load_a_h);
+    REGISTER_OPCODE(0x7D, op_load_a_l);
+    REGISTER_OPCODE(0x7E, op_load_a_hlp);
+    REGISTER_OPCODE(0x7F, op_load_a_a);
 }
 
 void cpu_step(CPU* cpu) {
+    if (cpu->halted) {
+        cpu->cycles += 4; // maybe do nothing or wait for an interrupt
+        return;
+    }
+
     uint8_t opcode = mem_read(cpu->pc++);
     execute_opcode(cpu, opcode);
 }
